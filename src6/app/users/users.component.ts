33efe52a0c3808e22533a5350d531ffe70d9c73d
HUSNA805 <husna80005@gmail.com>
2019-09-27 20:19:27 +0530

    angular6

---
+++ src6/app/users/users.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FirebaseService } from '../firebase.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  constructor(private firebaseService: FirebaseService, private router: Router) { }
+
+  updateUser(user){
+  this.firebaseService.selectedUser = user;
+  this.router.navigateByUrl('/firebase');
+
+}  
+
+  ngOnInit() {
+    this.firebaseService.getData();
+    console.log(this.firebaseService.users);
+  }
+
+}
